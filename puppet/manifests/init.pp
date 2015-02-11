35eb392181e43d08f53c642b2f2224c2f01c19c1
Zoltan Simon <zoltan@zoltansimon.me>
2015-02-11 20:36:06 +0100

    fix mailcatcher upstart

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,64 +1,66 @@
 Exec {
-  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/local/bin']
+  path => ['/bin', '/sbin', '/usr/bin', '/usr/sbin', '/usr/local/bin'],
 }
 
 Package {
-  ensure => 'present'
+  ensure => 'present',
 }
 
 exec { 'apt-update':
-  command => '/usr/bin/apt-get update -qq',
+  command => '/usr/bin/apt-get update -y',
 }
 
 # ensure base packages
-$packages = ['build-essential', 'keychain', 'git','curl', 'libxml2', 'libxml2-dev', 'libxslt1-dev']
+$packages = ['build-essential', 'keychain', 'git','curl', 'libxml2', 'libxml2-dev', 'libxslt1-dev', 'pwgen']
 
 package { $packages:
   require => Exec['apt-update'],
 }
 
-# node
-
-class { 'nodejs':
-  manage_repo => true,
-  require => Exec['apt-update'],
+# apache
+class { 'apache':
+  process_user => 'vagrant',
+  require      => Exec['apt-update'],
 }
 
-exec { 'npm-post-install':
-  command => 'npm install -g npm && sudo npm -g config set prefix /home/vagrant/npm',
-  require => Class['nodejs'],
+apache::dotconf { 'custom':
+  content => 'EnableSendfile Off',
 }
 
-file {'/etc/profile.d/nodejs.sh':
-  ensure => 'absent',
-  require => Class['nodejs'],
+apache::module { 'rewrite': }
+
+apache::vhost { '000-default':
+  priority                 => '',
+  docroot                  => '/var/www',
+  directory                => '/var/www',
+  directory_allow_override => 'All',
+  aliases                  => ['/adminer /usr/share/adminer'],
 }
 
-profile::script { 'npm':
-  priority => '050',
-  content => template('/vagrant/puppet/templates/npm.sh.erb'),
-  require => Exec['npm-post-install'],
+file { '/var/lock/apache2':
+  ensure => directory,
+  owner  => vagrant
 }
 
-define install_npm_package {
-  exec { "npm install -g $name":
-    cwd => '/home/vagrant',
-    user => 'vagrant',
-    require => Profile::Script['npm'],
-  }
+exec { 'ApacheUserChange' :
+  command => "sed -i 's/export APACHE_RUN_USER=.*/export APACHE_RUN_USER=vagrant/ ; s/export APACHE_RUN_GROUP=.*/export APACHE_RUN_GROUP=vagrant/' /etc/apache2/envvars",
+  require => [ Package['apache'], File['/var/lock/apache2'] ],
+  notify  => Service['apache'],
 }
-$npm_packages = ['yo','generator-angular', 'generator-bootstrap', 'generator-meanjs','generator-gruntfile','serve','bower','grunt-cli']
 
-install_npm_package { $npm_packages : }
+package {'apache2-utils':
+  require => Package['apache'],
+}
 
 # php
 
 $phpModules = [ 'fpm','imagick', 'xdebug', 'curl', 'mysql', 'cli', 'intl', 'mcrypt', 'memcache','gd']
 
 class { 'php':
-  service => 'apache',
-  notify  => Service["apache"],
-  require => Exec['apt-update'],
+  service         => 'apache',
+  install_options => [],
+  notify          => Service['apache'],
+  require         => [ Package['apache'], Exec['apt-update'] ],
 }
 
 php::module { $phpModules: }
@@ -67,11 +69,11 @@ php::module { $phpModules: }
 exec { 'enable_mcrypt':
   command => 'php5enmod mcrypt',
   require => Package['php5-mcrypt'],
-  notify  => Service["apache"],
+  notify  => Service['apache'],
 }
 
 $php_changes = [
-	'set Date/date.timezone "Europe/Copenhagen"',
+    'set Date/date.timezone "Europe/Copenhagen"',
     'set PHP/memory_limit 128M',
     'set PHP/post_max_size 128M',
     'set PHP/upload_max_filesize 128M',
@@ -89,102 +91,72 @@ $php_changes = [
     'set Phar/phar.readonly Off'
 ]
 
-augeas {  'php5-apache-ini':
-  context   => '/files/etc/php5/apache2/php.ini',
-  changes   => $php_changes,
-  require => Package["php5"],
-  notify  => Service["apache"],
+augeas { 'php5-apache-ini':
+  context => '/files/etc/php5/apache2/php.ini',
+  changes => $php_changes,
+  require => Package['php5'],
+  notify  => Service['apache'],
 }
 
-augeas {  'php5-cli-ini':
-  context   => '/files/etc/php5/cli/php.ini',
-  changes   => $php_changes,
-  require => Package["php5"],
-  notify  => Service["apache"],
+augeas { 'php5-cli-ini':
+  context => '/files/etc/php5/cli/php.ini',
+  changes => $php_changes,
+  require => Package['php5'],
+  notify  => Service['apache'],
 }
 
 # php-xdebug
 augeas { 'php5-xdebug':
-  context   => '/files/etc/php5/mods-available/xdebug.ini',
-  changes   => ['set XDebug/xdebug.remote_enable 1',
-                'set XDebug/xdebug.remote_port 9000',
+  context => '/files/etc/php5/mods-available/xdebug.ini',
+  changes => ['set XDebug/xdebug.remote_enable 1',
                 'set XDebug/xdebug.profiler_enable 1',
                 'set XDebug/xdebug.profiler_output_dir "/tmp"',
                 'set XDebug/xdebug.max_nesting_level 400'],
-  require => Package["php5-xdebug"],
-  notify  => Service["apache"],
+  require => Package['php5-xdebug'],
+  notify  => Service['apache'],
 
 }
 
 #  var/www symlink
 
-file { "/vagrant":
-  ensure  => "directory",
-  require => Package["php5"],
-  before => File[ '/var/www'],
+file { '/vagrant':
+  ensure  => 'directory',
+  require => Package['php5'],
+  before  => File[ '/var/www'],
 }
 
 file { '/var/www':
-  ensure  => "link",
-  target  => "/vagrant",
+  ensure  => 'link',
+  target  => '/vagrant',
   require => File['/vagrant'],
-  notify  => Service["apache"],
+  notify  => Service['apache'],
   force   => true,
 }
 
-# apache
-class { 'apache':
-  process_user => 'vagrant',
-}
+# mysql
 
-apache::dotconf { 'custom':
-  content => 'EnableSendfile Off',
-}
-
-apache::module { 'rewrite': }
-
-apache::vhost { '000-default':
-   priority                  => '',
-   docroot                   => '/var/www',
-   directory                 => '/var/www',
-   directory_allow_override  => 'All',
-   aliases 					 => ['/adminer /usr/share/adminer']
-}
-
-file { "/var/lock/apache2":
-  ensure => directory,
-  owner => vagrant
-}
-
-exec { "ApacheUserChange" :
-  command => "sed -i 's/export APACHE_RUN_USER=.*/export APACHE_RUN_USER=vagrant/ ; s/export APACHE_RUN_GROUP=.*/export APACHE_RUN_GROUP=vagrant/' /etc/apache2/envvars",
-  require => [ Package["apache"], File["/var/lock/apache2"] ],
-  notify  => Service['apache'],
-}
-# mysql 
-
-class { "mysql":
+class { 'mysql':
   root_password => 'mask',
 }
 
 mysql::grant { 'vagrant':
-  mysql_user => 'vagrant',
-  mysql_password => 'vagrant',
+  mysql_user       => 'vagrant',
+  mysql_password   => 'vagrant',
   mysql_privileges => 'ALL',
-  mysql_db => '*',
+  mysql_db         => '*',
 }
 
 file { '/home/vagrant/.my.cnf' :
-	ensure => present,
-    content => template('/vagrant/puppet/templates/my.cnf.erb'),
-    owner => 'vagrant',
-    group => 'vagrant',  
+  ensure  => present,
+  content => template('/vagrant/puppet/templates/my.cnf.erb'),
+  owner   => 'vagrant',
+  group   => 'vagrant',
 }
 
 # drush
 
-package {'drush':
-  require => Package['php'],
+package { 'drush':
+  require => Package['php5'],
 }
 
 exec { "drush dl drush --destination='/usr/share'":
@@ -192,8 +164,8 @@ exec { "drush dl drush --destination='/usr/share'":
 }
 
 exec { 'wget https://raw.githubusercontent.com/drush-ops/drush/master/drush.complete.sh':
-  cwd => '/etc/bash_completion.d',
-  creates => "/etc/bash_completion.d/drush.complete.sh",
+  cwd     => '/etc/bash_completion.d',
+  creates => '/etc/bash_completion.d/drush.complete.sh',
   require => Package['drush'],
 }
 
@@ -201,113 +173,140 @@ exec { 'wget https://raw.githubusercontent.com/drush-ops/drush/master/drush.comp
 
 exec { 'wpcli_install':
   command => 'curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && sudo mv wp-cli.phar /usr/local/bin/wp && sudo chmod a+x /usr/local/bin/wp',
-  require => Package['curl', 'php'],
+  require => Package['curl', 'php5'],
 }
 
 # composer
 
 exec { 'composer_install':
   command => 'curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer && sudo chmod a+x /usr/local/bin/composer',
-  require => Package['curl', 'php'],
+  require => Package['curl', 'php5'],
 }
 
 # adminer
 
 file { '/usr/share/adminer':
-  replace => no,
   ensure  => directory,
-  owner => 'vagrant',
-  group => 'www-data',
-  mode    => 775,
+  replace => no,
+  owner   => 'vagrant',
+  group   => 'www-data',
+  mode    => '0775',
   recurse => true,
-  require => Package['php']
+  require => Package['php5'],
 }
 
 exec{ 'wget http://www.adminer.org/latest.php -O /usr/share/adminer/index.php':
   require => File['/usr/share/adminer'],
-  creates => "/usr/share/adminer/index.php",
+  creates => '/usr/share/adminer/index.php',
   returns => [ 0, 4 ],
 }
 
 # ruby & rbenv
 
-rbenv::install { "vagrant":
+rbenv::install { 'vagrant':
   group => 'vagrant',
   home  => '/home/vagrant',
-  rc   => ".bash_profile",
+  rc    => '.bash_profile',
 }
 
-rbenv::compile { "2.1.4":
-  user => "vagrant",
-  home => "/home/vagrant",
+rbenv::compile { '2.1.4':
+  user   => 'vagrant',
+  home   => '/home/vagrant',
   global => true,
 }
 
-rbenv::gem { "jekyll":
-  user => "vagrant",
-  ruby => "2.1.4",
+rbenv::gem { 'jekyll':
+  user => 'vagrant',
+  ruby => '2.1.4',
 }
 
-rbenv::gem { "compass":
-  user => "vagrant",
-  ruby => "2.1.4",
+rbenv::gem { 'compass':
+  user => 'vagrant',
+  ruby => '2.1.4',
 }
 
-#rbenv::gem { "mailcatcher":
-#  user => "vagrant",
-#  ruby => "2.1.4",
-#  source => "https://github.com/sj26/mailcatcher"
-#}
-
-# package { 'compass':
-#  ensure   => 'installed',
-#provider => 'gem',
-# }
-
-# mailcatcher
-
-package { 'mailcatcher':
-  ensure   => 'present',
-  provider => 'gem',
+rbenv::gem { 'yaml-lint':
+  user => 'vagrant',
+  ruby => '2.1.4',
 }
 
-  file {'/etc/init/mailcatcher.conf':
-    ensure  => 'file',
-    content => template('/vagrant/puppet/templates/mailcatcher.conf.erb'),
-    mode    => '0755',
-    require => Package['mailcatcher'],
-    notify  => Service['mailcatcher'],
-  }
+rbenv::gem { 'mailcatcher':
+  user   => 'vagrant',
+  ruby   => '2.1.4',
+  source => 'https://github.com/sj26/mailcatcher',
+}
 
-  file {'/var/log/mailcatcher':
-    ensure  => 'directory',
-    owner   => 'vagrant',
-    group   => 'vagrant',
-    mode    => '0755',
-  }
-  
-  service {'mailcatcher':
-    ensure     => 'running',
-    provider   => 'upstart',
-    hasstatus  => true,
-    hasrestart => true,
-    require    => File['/etc/init/mailcatcher.conf'],
-  }
+file { '/etc/init/mailcatcher.conf':
+  ensure  => 'file',
+  content => template('/vagrant/puppet/templates/mailcatcher.conf.erb'),
+  mode    => '0755',
+  require => Rbenv::Gem['mailcatcher'],
+  notify  => Service['mailcatcher'],
+}
+
+file { '/var/log/mailcatcher':
+  ensure  => 'directory',
+  owner   => 'vagrant',
+  group   => 'vagrant',
+  mode    => '0755',
+}
+
+service { 'mailcatcher':
+  ensure     => 'running',
+  provider   => 'upstart',
+  hasstatus  => true,
+  hasrestart => true,
+  require    => File['/etc/init/mailcatcher.conf'],
+}
 
 # dotfiles
 
-exec { "git clone https://github.com/zsim0n/dotfiles.git && cd /home/vagrant/dotfiles && chmod +x ./bootstrap.sh && ./bootstrap.sh -f && rm -Rf /home/vagrant/dotfiles":
-  cwd => '/home/vagrant',
+exec { 'git clone https://github.com/zsim0n/dotfiles.git && cd /home/vagrant/dotfiles && chmod +x ./bootstrap.sh && ./bootstrap.sh -f && rm -Rf /home/vagrant/dotfiles':
+  cwd  => '/home/vagrant',
   user => 'vagrant',
 }
+
+# node
+
+class { 'nodejs':
+  manage_repo => true,
+  require     => Exec['apt-update'],
+}
+
+exec { 'npm-post-install':
+  command => 'npm install -g npm && sudo npm -g config set prefix /home/vagrant/npm',
+  require => Class['nodejs'],
+}
+
+file { '/etc/profile.d/nodejs.sh':
+  ensure  => 'absent',
+  require => Class['nodejs'],
+}
+
+profile::script { 'npm':
+  priority => '050',
+  content  => template('/vagrant/puppet/templates/npm.sh.erb'),
+  require  => Exec['npm-post-install'],
+}
+
+define install_npm_package {
+  exec { "npm install -g $name":
+    cwd     => '/home/vagrant',
+    user    => 'vagrant',
+    require => Profile::Script['npm'],
+  }
+}
+$npm_packages = ['yo','generator-angular', 'generator-bootstrap', 'generator-meanjs','generator-gruntfile','serve','bower','grunt-cli']
+
+install_npm_package { $npm_packages : }
+
 
 # mondgodb
 
 class {'::mongodb::globals':
   manage_package_repo => true,
 }->
-class {'::mongodb::server': 
+class {'::mongodb::server':
   config => '/etc/mongod.conf',
 }->
 class {'::mongodb::client': }
-
